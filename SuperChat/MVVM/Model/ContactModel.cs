528e13c253a3485d32f97b1a6c1751333b8b43a5
Gabriel-HANSEN <vox_idiomas@hotmail.com>
2022-09-24 21:37:14 -0300

    Primeiros codigos da MainViewModel.cs

--- SuperChat/MVVM/Model/ContactModel.cs
+++ SuperChat/MVVM/Model/ContactModel.cs
@@ -12,5 +12,7 @@ namespace SuperChat.MVVM.Model
         public string Username { get; set; }
         public string ImageSource { get; set; }
         public ObservableCollection<MessageModel> Messages { get; set; }
+
+        public string LastMessage => Messages.Last().Message;
     }
 }
--- SuperChat/MVVM/ViewModel/MainViewModel.cs
+++ SuperChat/MVVM/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SuperChat.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,7 @@ namespace SuperChat.MVVM.ViewModel
 {
     internal class MainViewModel
     {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
     }
 }
